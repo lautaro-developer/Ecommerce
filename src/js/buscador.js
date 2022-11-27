c968d4ed938b21c7c777e06d9c6ac7e6b044a79a
lautaro <altairdiaz245@gmail.com>
2022-11-27 00:31:00 -0300

    a

--- src/js/buscador.js
+++ src/js/buscador.js
@@ -1,21 +1,7 @@
 const focud = document.querySelector(".inputBuscar__item");
-const ti = document.querySelector(".titulo");
-const tia = document.querySelectorAll(".li");
-console.log(tia);
+const ti2 = document.querySelector(".titulo");
+const tia = document.querySelectorAll("[data-list]");
+const ti = document.querySelectorAll("li");
 
-tia.forEach((v) => v.addEventListener("click", (a) => console.log(a.target)));
 
-const fun = () => {
-  const menu = ti.classList.toString();
-  if (menu == "titulo") {
-    ti.classList.remove("titulo");
-    ti.classList.add("false");
-  } else if (menu == "false") {
-    ti.classList.add("titulo");
-    ti.classList.remove("false");
-  }
-};
-
-focud.addEventListener("focus", fun);
-focud.addEventListener("blur", fun);
-focud.addEventListener("mouseover", () => console.log("sobre el"));
+console.log(tia)
